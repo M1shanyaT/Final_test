717cdc893816732b0b96497ada9efcb82feab137
mishaTytin <maliort165@gmail.com>
2023-12-12 14:23:00 +0300

    Создание текстового файла и функции для ввода массива

---
+++ final_test_task/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] res = GetArray(5);
+Console.WriteLine($"Первоначальный массив: [{String.Join("; ", res)}]");
+string[] GetArray(int size)
+{
+string[] array = new string[size]; 
+for (int i = 0; i < size; i++) 
+{
+Console.Write($"Введите {i} элемент массива: ");
+array[i] = Console.ReadLine();
+}
+return array;
+}
